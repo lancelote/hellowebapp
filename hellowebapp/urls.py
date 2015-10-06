d9127de111c5dc870da8a542acbc96c6e5fd5f46
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-10-06 11:31:15 +0300

    About and contact pages, base template update

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'collection.views.index', name='home'),
+    url(r'^about/', TemplateView.as_view(template_name='layouts/about.html'), name='about'),
+    url(r'^contact/', TemplateView.as_view(template_name='layouts/contact.html'), name='contact'),
 ]
