a1a7c558c4ebb4f3f508dba90baaa397e0af4a13
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-10-08 13:36:02 +0300

    View Thing objects on index page

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from collection.models import Thing
+
 
 def index(request):
-    return render(request, 'index.html')
+    things = Thing.objects.all()
+    return render(request, 'index.html', {
+        'things': things,
+    })
