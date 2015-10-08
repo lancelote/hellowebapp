212165f24ccb2615162316511e455a31978f974a
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-10-08 11:39:58 +0300

    Thing model and initial migration

---
+++ collection/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Thing',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255)),
+                ('description', models.TextField()),
+                ('slug', models.SlugField(unique=True)),
+            ],
+        ),
+    ]
